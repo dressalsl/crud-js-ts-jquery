7d56ef64df212a726487d945ffe1180876d2457f
Andressa <dressalsl97@gmail.com>
2020-01-02 00:42:55 -0300

    add funcoes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,51 @@
 let salvar = () => {
 
-let dados =localStorage.info == null?[]:JSON.parse(localStorage.info);
+    let dados =localStorage.info == null?[]:JSON.parse(localStorage.info);
 
-let documento = document.querySelector("#txtDocuemnto").value;
-let nome = document.querySelector("#txtNome").value;
-let email = document.querySelector("#txtEmail").value;
-let cor = document.querySelector("#txtCor").value;
+    let documento = document.querySelector("#txtDocuemnto").value;
+    let nome = document.querySelector("#txtNome").value;
+    let email = document.querySelector("#txtEmail").value;
+    let cor = document.querySelector("#txtCor").value;
 
-// 14:47 https://www.youtube.com/watch?v=rwnhpvfWgIQ&list=WL&index=35&t=0s
+    dados.push ({
+        'documento' : documento,
+        'nome' : nome,
+        'email' :email,
+        'cor' : cor
+    });
+
+    localStorage.info = JSON.stringify(dados);
+
+    alert("pegou")
+} 
+
+let lista = () => {
+    let dados =localStorage.info == null?[]:JSON.parse(localStorage.info);
+    let tabela = document.querySelector("#tblDados");
+    dados.forEach(element => {
+        tabela.innerHTML += `
+            <tr>
+                <td>${element.documento}</td>
+                <td>${element.nome}</td>
+                <td>${element.email}</td>
+                <td slyle = "background-color:${element.cor}'>${element.documento}</td>
+                <td>
+                    <button class="btn btn-primary" onclick="editar(${element.documento})>Editar</button>
+                    <button class="btn btn-danger>Excluir</button>
+                </td>
+            </tr>
+        `;
+    });
+}
+
+let editar = (documento) => {
+    let dados =localStorage.info == null?[]:JSON.parse(localStorage.info);
+
+    let resultado = dados.find(e => e.documento == documento);
+    let resultadoIndex =  dados.findIndex(e => e.documento == documento);
+    if (resultado != undefined){
+    }else{
+        alert("Não foi encontrado")
+    }
+    //  24:56 https://www.youtube.com/watch?v=rwnhpvfWgIQ&t=617s
+}
